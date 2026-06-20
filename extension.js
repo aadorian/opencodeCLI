f1d2c8c97641322e113b3fd00e4c98fad4f30faf
Ale <aadorian@gmail.com>
2026-06-20 15:30:27 -0300

    feat: add activity bar button to launch walkthrough
    - New OpenCode icon in the left activity bar
    - Welcome panel with 'Open Walkthrough' button
    - Links to documentation and issue tracker

--- extension.js
+++ extension.js
@@ -1,13 +1,18 @@
 const vscode = require('vscode');
 
 function activate(context) {
-  const disposable = vscode.commands.registerCommand('opencode-walkthrough.showWalkthrough', () => {
+  const showWalkthrough = vscode.commands.registerCommand('opencode-walkthrough.showWalkthrough', () => {
     vscode.commands.executeCommand('workbench.action.openWalkthrough', {
       walkthroughID: 'opencode-walkthrough.opencode.gettingStarted'
     });
   });
 
-  context.subscriptions.push(disposable);
+  const treeProvider = vscode.window.registerTreeDataProvider('opencode-welcome', {
+    getChildren: () => [],
+    getTreeItem: () => null
+  });
+
+  context.subscriptions.push(showWalkthrough, treeProvider);
 }
 
 function deactivate() {}
